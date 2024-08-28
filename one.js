afaea09eef515b4b6b25f95c13582632a8e9a2c6
charan9515 <135000144+charan9515@users.noreply.github.com>
2024-08-28 09:56:16 +0530

    Add files via upload
    ok

---
+++ one.js
@@ -0,0 +1,55 @@
+var input = document.getElementById('input');
+var result = document.getElementById('result');
+var inputType = document.getElementById('inputType');
+var resultType = document.getElementById('resultType');
+var option_from,option_to;
+
+input.addEventListener("keyup",myResult);
+inputType.addEventListener("change",myResult);
+resultType.addEventListener("change",myResult);
+option_from = inputType.value;
+option_to   = resultType.value;
+
+
+function myResult(){
+
+	option_from = inputType.value;
+	option_to = resultType.value;
+
+	if(option_from === "meter" && option_to==="kilometer"){ 
+		result.value = Number(input.value) * 0.001;
+	}else if(option_from === "meter" && option_to==="Centimeter"){
+		result.value = Number(input.value) * 100;
+
+	}else if(option_from === "meter" && option_to==="meter"){
+		result.value = input.value
+	}
+
+
+	if(option_from === "kilometer" && option_to==="meter"){
+		result.value = Number(input.value) * 1000;
+	}else if(option_from === "kilometer" && option_to==="Centimeter"){
+		//this is kilometer to Centimeter formula 
+		result.value = Number(input.value) * 100000;
+	}else if(option_from === "kilometer" && option_to==="kilometer"){
+		//this is kilometer to kilometer formula 
+		result.value = input.value
+	}
+
+	if(option_from === "Centimeter" && option_to==="kilometer"){
+		//this is Centimeter to kilometer formula 
+		result.value = Number(input.value) * 0.00001;
+	}else if(option_from === "Centimeter" && option_to==="meter"){
+		//this is Centimeter to meter formula 
+		result.value = Number(input.value) * 0.01;
+	}else if(option_from === "Centimeter" && option_to==="Centimeter"){
+		//this is Centimeter to Centimeter formula 
+		result.value = input.value
+	}
+	
+
+
+
+
+
+}
